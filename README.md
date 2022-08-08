33b52e6913b5103ccf9ea6a07b70da359b09f62b
ivanstarodubtcev <loteran@icloud.com>
2022-08-08 22:45:11 +0300

    add eslint, fix lint bags, add code climate maintability bage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/Starodubtcev/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Starodubtcev/frontend-project-lvl2/actions)
+
+<a href="https://codeclimate.com/github/Starodubtcev/frontend-project-lvl2/maintainability"><img src="https://api.codeclimate.com/v1/badges/ebd5b4f66a820ce8da9e/maintainability" /></a>
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-    .version ('0.0.1','-v, --version', 'Compares two configuration files and shows a difference.' )
-    .description('Compares two configuration files and shows a difference.');
+  .version('0.0.1', '-v, --version', 'Compares two configuration files and shows a difference.')
+  .description('Compares two configuration files and shows a difference.');
 
 program.parse();
