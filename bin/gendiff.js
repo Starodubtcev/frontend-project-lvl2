7f3c6351916a62d5c213cc8041a894b17f6281ea
ivanstarodubtcev <loteran@icloud.com>
2022-08-08 22:23:17 +0300

    fix bin/gendiff.js 1 step finish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
 
 const program = new Command();
 
 program
-    .version ('0')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
-    .option('-h, --help', 'display help for command');
+    .version ('0.0.1','-v, --version', 'Compares two configuration files and shows a difference.' )
+    .description('Compares two configuration files and shows a difference.');
 
 program.parse();
