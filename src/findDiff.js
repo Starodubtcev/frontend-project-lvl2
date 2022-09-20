92cec79699d3d735d782fb86f958bec502ce1209
ivanstarodubtcev <loteran@icloud.com>
2022-09-20 20:39:14 +0300

    refactoring before 1st check v2

--- src/findDiff.js
+++ src/findDiff.js
@@ -8,41 +8,22 @@ const getOneLevelDiff = (dataParsed1, dataParsed2) => {
   const keys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(keys);
   const difference = sortedKeys.map((item) => {
+    let resultObject = {};
     if (_.isObject(dataParsed1[item]) && _.isObject(dataParsed2[item])) {
-      return {
-        name: item,
-        value: getOneLevelDiff(dataParsed1[item], dataParsed2[item]),
-        type: 'nested',
-      };
-    }
-    if (!_.isEqual(dataParsed1[item], dataParsed2[item])
+      resultObject = { name: item, value: getOneLevelDiff(dataParsed1[item], dataParsed2[item]), type: 'nested' };
+    } else if (!_.isEqual(dataParsed1[item], dataParsed2[item])
     && _.has(dataParsed1, item) && _.has(dataParsed2, item)) {
-      return {
-        name: item,
-        value1: dataParsed1[item],
-        value2: dataParsed2[item],
-        type: 'changed',
+      resultObject = {
+        name: item, value1: dataParsed1[item], value2: dataParsed2[item], type: 'changed',
       };
+    } else if (!_.has(dataParsed1, item)) {
+      resultObject = { name: item, value: dataParsed2[item], type: 'added' };
+    } else if (!_.has(dataParsed2, item)) {
+      resultObject = { name: item, value: dataParsed1[item], type: 'deleted' };
+    } else {
+      resultObject = { name: item, value: dataParsed1[item], type: 'unchanged' };
     }
-    if (!_.has(dataParsed1, item)) {
-      return {
-        name: item,
-        value: dataParsed2[item],
-        type: 'added',
-      };
-    }
-    if (!_.has(dataParsed2, item)) {
-      return {
-        name: item,
-        value: dataParsed1[item],
-        type: 'deleted',
-      };
-    }
-    return {
-      name: item,
-      value: dataParsed1[item],
-      type: 'unchanged',
-    };
+    return resultObject;
   });
   return difference;
 };
